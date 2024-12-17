25640375ea586bb11792aec36227f24d63e2a6db
sieep-coding <nstamb6@gmail.com>
2024-12-17 14:48:27 -0500

    cleanup crew 🧹

--- tests/utils/logger.test.ts
+++ tests/utils/logger.test.ts
@@ -1,12 +1,15 @@
 import { Logger } from '../../src/utils/logger';
 import chalk from "chalk";
 
+const errorMessage = chalk.red('[ERROR]: Test log message');
+const testMessage = chalk.cyan('[INFO]: Test log message')
+
 describe("Logger", () => {
     it("should always log messages successfully.", () => {
         const logger = new Logger();
         console.log = jest.fn();
         logger.info('Test log message')
-        expect(console.log).toHaveBeenCalledWith(chalk.cyan('[INFO]: Test log message'));
+        expect(console.log).toHaveBeenCalledWith(testMessage);
     });
 });
 
@@ -15,6 +18,6 @@ describe("Logger-Error", () => {
         const logger = new Logger();
         console.error = jest.fn();
         logger.error('Test log message')
-        expect(console.error).toHaveBeenCalledWith(chalk.red('[ERROR]: Test log message'));
+        expect(console.error).toHaveBeenCalledWith(errorMessage);
     });
 });
