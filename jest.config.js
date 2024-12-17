f02ed41457b4eea0be405e261bdd80b07622a5b0
sieep-coding <nstamb6@gmail.com>
2024-12-17 11:58:18 -0500

    format

--- jest.config.js
+++ jest.config.js
@@ -2,12 +2,16 @@
 export default {
     preset: "ts-jest",
     testEnvironment: "node",
+    transformIgnorePatterns: [
+      '/node_modules/(?!chalk)/',
+    ],
     transform: {
       "^.+\\.tsx?$": "ts-jest",
     },
     moduleFileExtensions: ["ts", "js", "json"],
     globals: {
       "ts-jest": {
+        useESM: true,
         tsconfig: "tsconfig.test.json",
       },
     },
