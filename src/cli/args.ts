94a9bea3424874a0a4f3a54580afbed51927d9a9
sieep-coding <nstamb6@gmail.com>
2024-12-17 11:03:11 -0500

    misspelled and build update

--- src/cli/args.ts
+++ src/cli/args.ts
@@ -5,7 +5,7 @@ export const parseArgs = () => {
     return yargs(hideBin(process.argv))
         .usage("Usage: $0 --data [comma-separated values]")
         .option("data", {
-            describe: "Comma-seperated list of numbers to visualize in the console.",
+            describe: "Comma-separated list of numbers to visualize in the console.",
             type: "string",
             demandOption: true,
         })
